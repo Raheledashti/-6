f23a6fc8fc8c7b3c38cb2ffbdc6b67e399ca4a27
Raheledashti <dashtirahele751@gmail.com>
2024-09-27 14:17:23 +0330

    Add files via upload

---
+++ main (8).cs
@@ -0,0 +1,12 @@
+using system;
+namespace Firstprogram{
+    class program{
+        static voidMain(string[]args){
+        string x=console.ReadLine();
+        string y=console.ReadLine();
+        int sum=Convert.Tolnt32(x)+Covert.Tolnt32(y);
+        Console.WriteLine(sum);
+            
+        }
+    }
+}
